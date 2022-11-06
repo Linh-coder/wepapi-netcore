af0f42e75f7b69c3a852bde2d8ea691e4f346daa
Linh <Linhtran2391999@gmail.com>
2022-11-06 10:58:35 +0700

    02_WorkingDB

---
+++ webApiApp/webApiApp/Controllers/HangHoaController.cs
@@ -0,0 +1,84 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using webApiApp.Models;
+using System.Linq;
+
+namespace webApiApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HangHoaController : ControllerBase
+    {
+        public static List<HangHoa> hangHoas = new List<HangHoa>();
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            return Ok(hangHoas);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetById(string id)
+        {
+            try
+            {
+                var hangHoa = hangHoas.SingleOrDefault(x => x.Code == Guid.Parse(id));
+                if (hangHoa == null)
+                {
+                    return NotFound();
+                }
+                return Ok(hangHoas);
+            }
+            catch
+            {
+                return BadRequest();
+            }
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult Edit(string id, HangHoa hanghoaEdit)
+        {
+            try
+            {
+                var hangHoa = hangHoas.SingleOrDefault(x => x.Code == Guid.Parse(id));
+                if (hangHoa == null)
+                {
+                    return NotFound();
+                }
+                if (id != hangHoa.Code.ToString())
+                {
+                    return BadRequest();
+                }
+                hangHoa.Name = hanghoaEdit.Name;
+                hangHoa.Price = hanghoaEdit.Price;
+                return Ok(hangHoas);
+            }
+            catch
+            {
+                return BadRequest();
+            }
+        }
+
+        [HttpPost]
+        public IActionResult Create(HangHoaVM hangHoaVM)
+        {
+            var hanghoa = new HangHoa()
+            {
+                Name = hangHoaVM.Name,
+                Price = hangHoaVM.Price,
+                Code = Guid.NewGuid()
+            };
+            hangHoas.Add(hanghoa);
+            return Ok(new
+            {
+                Success = true,
+                Data = hanghoa
+            });
+
+        }
+
+
+    }
+}
